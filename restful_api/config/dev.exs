35fb4da975cf57fc53dcf26a461c170189be6c92
wang1990cool <wang1990cool@hotmail.com>
2018-03-25 21:14:19 +0800

    alter dev.exs

--- restful_api/config/dev.exs
+++ restful_api/config/dev.exs
@@ -52,7 +52,7 @@ config :phoenix, :stacktrace_depth, 20
 config :restful_api, RestfulApi.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "root",
   database: "restful_api_dev",
   hostname: "localhost",
   pool_size: 10
