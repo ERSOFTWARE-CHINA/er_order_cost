d3aff3c05d007fd9954ad020fef8f7cb101a5c1a
tms <threemonoliths@gmail.com>
2018-03-27 17:59:37 +0800

    heartbeat

--- restful_api/lib/restful_api/sparepart_service/sparepart_service.ex
+++ restful_api/lib/restful_api/sparepart_service/sparepart_service.ex
@@ -1,104 +1,22 @@
 defmodule RestfulApi.SparepartService do
-  @moduledoc """
-  The SparepartService context.
-  """
-
   import Ecto.Query, warn: false
-  alias RestfulApi.Repo
-
+  use RestfulApi.BaseContext
   alias RestfulApi.SparepartService.Sparepart
 
-  @doc """
-  Returns the list of spareparts.
-
-  ## Examples
-
-      iex> list_spareparts()
-      [%Sparepart{}, ...]
-
-  """
-  def list_spareparts do
-    Repo.all(Sparepart)
+  defmacro __using__(_opts) do
+    quote do
+      import RestfulApi.SparepartService
+      use RestfulApi.BaseContext
+      alias RestfulApi.SparepartService.Sparepart
+    end
   end
 
-  @doc """
-  Gets a single sparepart.
-
-  Raises `Ecto.NoResultsError` if the Sparepart does not exist.
-
-  ## Examples
-
-      iex> get_sparepart!(123)
-      %Sparepart{}
-
-      iex> get_sparepart!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_sparepart!(id), do: Repo.get!(Sparepart, id)
-
-  @doc """
-  Creates a sparepart.
-
-  ## Examples
-
-      iex> create_sparepart(%{field: value})
-      {:ok, %Sparepart{}}
-
-      iex> create_sparepart(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_sparepart(attrs \\ %{}) do
-    %Sparepart{}
-    |> Sparepart.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a sparepart.
-
-  ## Examples
-
-      iex> update_sparepart(sparepart, %{field: new_value})
-      {:ok, %Sparepart{}}
-
-      iex> update_sparepart(sparepart, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_sparepart(%Sparepart{} = sparepart, attrs) do
-    sparepart
-    |> Sparepart.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a Sparepart.
-
-  ## Examples
-
-      iex> delete_sparepart(sparepart)
-      {:ok, %Sparepart{}}
-
-      iex> delete_sparepart(sparepart)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_sparepart(%Sparepart{} = sparepart) do
-    Repo.delete(sparepart)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking sparepart changes.
-
-  ## Examples
-
-      iex> change_sparepart(sparepart)
-      %Ecto.Changeset{source: %Sparepart{}}
-
-  """
-  def change_sparepart(%Sparepart{} = sparepart) do
-    Sparepart.changeset(sparepart, %{})
+  def page(params, conn) do 
+    Sparepart
+    |> query_like(params, "name")
+    |> query_like(params, "attributes")
+    |> query_like(params, "specifications")
+    |> query_order_by(params, "name")
+    |> get_pagination(params, conn)
   end
 end
--- restful_api/lib/restful_api_web/controllers/sparepart_controller.ex
+++ restful_api/lib/restful_api_web/controllers/sparepart_controller.ex
@@ -1,18 +1,21 @@
 defmodule RestfulApiWeb.SparepartController do
   use RestfulApiWeb, :controller
-
+  use RestfulApi.SparepartService
   alias RestfulApi.SparepartService
   alias RestfulApi.SparepartService.Sparepart
+  import RestfulApiWeb.Plugs.Auth, only: [project_active: 2]
+
+  plug :project_active
 
   action_fallback RestfulApiWeb.FallbackController
 
-  def index(conn, _params) do
-    spareparts = SparepartService.list_spareparts()
-    render(conn, "index.json", spareparts: spareparts)
+  def index(conn, params) do
+    page = page(params, conn)
+    render(conn, "index.json", page: page)
   end
 
   def create(conn, %{"sparepart" => sparepart_params}) do
-    with {:ok, %Sparepart{} = sparepart} <- SparepartService.create_sparepart(sparepart_params) do
+    with {:ok, %Sparepart{} = sparepart} <- save_create(Sparepart.changeset(%Sparepart{}, sparepart_params), conn) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", sparepart_path(conn, :show, sparepart))
@@ -21,22 +24,22 @@ defmodule RestfulApiWeb.SparepartController do
   end
 
   def show(conn, %{"id" => id}) do
-    sparepart = SparepartService.get_sparepart!(id)
-    render(conn, "show.json", sparepart: sparepart)
-  end
-
-  def update(conn, %{"id" => id, "sparepart" => sparepart_params}) do
-    sparepart = SparepartService.get_sparepart!(id)
-
-    with {:ok, %Sparepart{} = sparepart} <- SparepartService.update_sparepart(sparepart, sparepart_params) do
+    with {:ok, sparepart} <- get_by_id(Sparepart, id, conn) do
       render(conn, "show.json", sparepart: sparepart)
     end
   end
 
+  def update(conn, %{"id" => id, "sparepart" => sparepart_params}) do
+    with {:ok, sparepart} <- get_by_id(Sparepart, id, conn, [:project]) do
+      with {:ok, %Sparepart{} = sparepart} <- save_update(Sparepart.changeset(sparepart, sparepart_params), conn) do
+        render(conn, "show.json", sparepart: sparepart)
+      end
+    end
+  end
+
   def delete(conn, %{"id" => id}) do
-    sparepart = SparepartService.get_sparepart!(id)
-    with {:ok, %Sparepart{}} <- SparepartService.delete_sparepart(sparepart) do
-      send_resp(conn, :no_content, "")
+    with {:ok, %Sparepart{} = sparepart} <- delete_by_id(Sparepart, id, conn) do
+      render(conn, "show.json", sparepart: sparepart)
     end
   end
 end
