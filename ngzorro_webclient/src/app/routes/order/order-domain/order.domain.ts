23e9a119ef8c65b943560bcb13735332031aa360
versnoon <versnoon@gmail.com>
2018-03-29 07:36:37 +0800

    完善订单模型

--- ngzorro_webclient/src/app/routes/order/order-domain/order.domain.ts
+++ ngzorro_webclient/src/app/routes/order/order-domain/order.domain.ts
@@ -1,4 +1,13 @@
+import { OrderDetail } from "./orderDetail.domain";
+
 export class Order {
   id: number;
   name: string;
+  pno: string;
+  price: number;
+  date: Date;
+  remark: string;
+  order: any;
+  details: OrderDetail[];
+  project_id: number;
 }
